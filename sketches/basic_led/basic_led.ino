b2dd30f199d205b32c375d9efdc47a9cf7590c88
FusedFormations <info@fusedformations.com>
2025-06-19 16:25:38 -0400

    Arduino sketch basic_led.ino - Web IDE upload

--- sketches/basic_led/basic_led.ino
+++ sketches/basic_led/basic_led.ino
@@ -1,7 +1,7 @@
 // Basic LED Blink Example for Arduino
 // Works with Arduino Uno, Mega, Nano, and other ATmega boards
 
-const int LED_PIN = 13; // Built-in LED on most Arduino boards
+const int LED_PIN = 5; // Built-in LED on most Arduino boards
 
 void setup() {
   // Initialize serial communication for debugging
