3425054ad7139666d5cdf47b3b8c6977c9757c91
FusedFormations <info@fusedformations.com>
2025-06-19 16:37:59 -0400

    Arduino sketch basic_led.ino - Web IDE upload

--- sketches/basic_led/basic_led.ino
+++ sketches/basic_led/basic_led.ino
@@ -6,18 +6,18 @@ void setup() {
   Serial.begin(115200);  // 57600 for old Nano bootloader
   
   // Set built-in LED as output
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(5, OUTPUT);
   
   Serial.println("Arduino LED Blink Started");
   Serial.println("Board: Arduino Uno/Nano");
 }
 
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);  // LED on
+  digitalWrite(5, HIGH);  // LED on
   Serial.println("LED ON");
   delay(1000);                      // Wait 1 second
   
-  digitalWrite(LED_BUILTIN, LOW);   // LED off
+  digitalWrite(5, LOW);   // LED off
   Serial.println("LED OFF");
   delay(1000);                      // Wait 1 second
 }
